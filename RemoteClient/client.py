dc1eb6dc5fa4815290b37d5415f0625fbd098ead
SteelPh0enix <steelphoenix2137@gmail.com>
2019-11-09 14:46:44 +0100

    more bruh

--- RemoteClient/client.py
+++ RemoteClient/client.py
@@ -2,6 +2,7 @@ import grpc
 from proto.Chassis_pb2 import ChassisData, ChassisFeedback
 from proto.Chassis_pb2_grpc import ChassisServiceStub
 from time import sleep
+from inputs import get_gamepad, devices
 import keyboard
 
 
@@ -10,29 +11,37 @@ GRPC_SERVER_PORT = '5000'
 GRPC_STUB = None
 CHASSIS_MAX_POWER = 150
 
+
 def test_comms(stub):
     for i in range(0, 10):
         response = stub.Drive(ChassisData(velocity=i*10, rotation=0))
         print(response)
         sleep(1)
 
-def drive(vel, rot):
-    print(GRPC_STUB.Drive(ChassisData(velocity=vel,rotation=rot)))
 
-def drive_forward():
+def drive(vel, rot):
+    print(GRPC_STUB.Drive(ChassisData(velocity=vel, rotation=rot)))
+
+
+def drive_forward(d):
     drive(CHASSIS_MAX_POWER, 0)
 
-def drive_backward():
+
+def drive_backward(d):
     drive(-CHASSIS_MAX_POWER, 0)
 
-def rotate_left():
+
+def rotate_left(d):
     drive(0, -CHASSIS_MAX_POWER)
 
-def rotate_right():
+
+def rotate_right(d):
     drive(0, CHASSIS_MAX_POWER)
 
-def stop_chassis():
-    drive(0,0)
+
+def stop_chassis(d):
+    drive(0, 0)
+
 
 def make_keybinds():
     keyboard.on_press_key('w', drive_forward)
@@ -42,15 +51,14 @@ def make_keybinds():
     keyboard.on_press_key('a', rotate_left)
     keyboard.on_release_key('a', stop_chassis)
     keyboard.on_press_key('d', rotate_right)
-    keyboard.on_release_key('s', stop_chassis)
+    keyboard.on_release_key('d', stop_chassis)
 
 
-def run():
-    with grpc.insecure_channel('{0}:{1}'.format(GRPC_SERVER_IP, GRPC_SERVER_PORT)) as channel:
-        stub = ChassisServiceStub(channel)
-        GRPC_STUB = stub
-        make_keybinds()
-        keyboard.wait()
-
-if __name__ == '__main__':
-    run()
+with grpc.insecure_channel('{0}:{1}'.format(GRPC_SERVER_IP, GRPC_SERVER_PORT)) as channel:
+    GRPC_STUB = ChassisServiceStub(channel)
+    while True:
+        events = get_gamepad()
+        for event in events:
+            print(event)
+    # make_keybinds()
+    # keyboard.wait()
